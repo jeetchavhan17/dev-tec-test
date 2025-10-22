f96ed47d6fdcaf73008c95de616757858cb536cb
jeetchavhan17 <jeet.o.india10@gmail.com>
2025-10-22 12:31:23 +0530

    outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -10,3 +10,10 @@ output "ecs_service_name" {
   value = aws_ecs_service.service.name
 }
 
+
+
+####################### Output #######################
+
+alb_dns = "devops-app-alb-1234567890.us-east-1.elb.amazonaws.com"
+ecs_cluster_name = "devops-sample-cluster"
+service_name = "devops-app-service"
